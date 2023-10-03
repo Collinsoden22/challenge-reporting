b0b30542d365e20dc5696a72d3757d00d9081775
Collinsoden22 <Collinsoden22@gmail.com>
2023-10-03 18:07:40 +0100

    Refactored api endpoints and formatted code style.

--- test.js
+++ test.js
@@ -21,52 +21,52 @@ tape('health', async function (t) {
 })
 
 tape('GET /student/:id should return a student by ID', async function (t) {
-   const url = `${endpoint}/student/2`
-  try{
+  const url = `${endpoint}/student/2`
+  try {
     const { data, response } = await jsonist.get(url)
     if (response.statusCode !== 200) {
-      throw new Error(`Error: ${data.body.error}`);
+      throw new Error(`Error: ${data.body.error}`)
     }
-    t.ok(data.success, 'Student information was retrieved.');
-    t.ok(data.body, 'Response body exist');
+    t.ok(data.success, 'Student information was retrieved.')
+    t.ok(data.body, 'Response body exist')
     t.end()
   } catch (e) {
-    t.error(e);
+    t.error(e)
   }
-});
+})
 
 tape('GET /student/:id/grades should return a student grades report by ID', async function (t) {
-   const url = `${endpoint}/student/1/grades`
-  try{
+  const url = `${endpoint}/student/1/grades`
+  try {
     const { data, response } = await jsonist.get(url)
     if (response.statusCode !== 200) {
       throw new Error(`Error: ${response.body.error}`)
     }
-    t.ok(data.success, 'Student information was retrieved.');
-    t.ok(data.body, 'Response body exist');
-    t.ok(data.body.student, 'Response contains student property');
-    t.ok(data.body.grades, 'Response contains grades property');
+    t.ok(data.success, 'Student information was retrieved.')
+    t.ok(data.body, 'Response body exist')
+    t.ok(data.body.student, 'Response contains student property')
+    t.ok(data.body.grades, 'Response contains grades property')
     t.end()
   } catch (e) {
-    t.error(e);
+    t.error(e)
   }
-});
+})
 
 tape('GET /course/all/grades should return course grades report', async function (t) {
-   const url = `${endpoint}/course/all/grades`
-  try{
+  const url = `${endpoint}/course/all/grades`
+  try {
     const { data, response } = await jsonist.get(url)
     if (response.statusCode !== 200) {
       throw new Error(`Error: ${response.body.error}`)
     }
-    t.ok(data.success, 'Course grades were retrieved.');
-    t.ok(data.body, 'Response body exist.');
-    t.ok(data.body.statistics, 'Response has statistics.');
+    t.ok(data.success, 'Course grades were retrieved.')
+    t.ok(data.body, 'Response body exist.')
+    t.ok(data.body.statistics, 'Response has statistics.')
     t.end()
   } catch (e) {
-    t.error(e);
+    t.error(e)
   }
-});
+})
 
 tape('cleanup', function (t) {
   server.closeDB()
